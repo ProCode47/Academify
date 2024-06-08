143f010a0619d041f4fb7eac18ddcc7c30bd3e53
procode47 <iloenyenwavictor@gmail.com>
2024-06-08 23:03:47 +0100

    updated connection string

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const config = {
   
     //Mongo DB
-    URI: "mongodb+srv://admin:d301f1wwKsoTO8bd@cluster0.qjeiurm.mongodb.net/?retryWrites=true&w=majority",
+    URI: "mongodb+srv://admin:d301f1wwKsoTO8bd@cluster0.qjeiurm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     secretKey:"8fe1c360af6948e6b82ff11ea9f3f8fd"
     
 };
